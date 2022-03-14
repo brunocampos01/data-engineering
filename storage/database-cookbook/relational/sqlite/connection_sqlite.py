ea80ae1889a26611b91e01166a4cf2c839cd9ee3
Bruno Campos <brunocampos01@gmail.com>
2022-03-14 02:55:45 -0300

    Update connection_sqlite.py

--- storage/database-cookbook/relational/sqlite/connection_sqlite.py
+++ storage/database-cookbook/relational/sqlite/connection_sqlite.py
@@ -2,6 +2,10 @@ import sqlite3
 
 
 conn = sqlite3.connect("users.db")
+
+# You can also supply the special name :memory: to create a temporary database in RAM
+# conn = sqlite3.connect(':memory:')
+
 c = conn.cursor()
 
 c.execute("CREATE TABLE user (name text, age integer)")
